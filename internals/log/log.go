5085dddb46131e1bdb5234f1a98598991f54e98d
arshpsps <arshpsps@gmail.com>
2026-04-22 09:01:14 +0530

    add: log/ NewLog(), Log.Append(), Log.Read()
    fix: format

--- internals/log/log.go
+++ internals/log/log.go
@@ -10,3 +10,34 @@ type Log struct {
 	messages []types.Message
 	mu       sync.RWMutex
 }
+
+func NewLog() *Log {
+	return &Log{
+		messages: make([]types.Message, 0),
+	}
+}
+
+func (l *Log) Append(payload []byte) int64 {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	offset := int64(len(l.messages))
+
+	msg := types.NewMessage(offset, payload)
+	l.messages = append(l.messages, msg)
+
+	return offset
+}
+
+func (l *Log) Read(from int64, max int) []types.Message {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	if from >= int64(len(l.messages)) {
+		return nil
+	}
+
+	end := min(from+int64(max), int64(len(l.messages)))
+
+	return l.messages[from:end]
+}
