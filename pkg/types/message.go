81e0f49d27d2061f23b5d715e9ce03bc8da4d485
arshpsps <arshpsps@gmail.com>
2026-04-22 08:46:29 +0530

    add: Message{}, NewMessage(), Message.Size()

---
+++ pkg/types/message.go
@@ -0,0 +1,21 @@
+package types
+
+import "time"
+
+type Message struct {
+	Offset int64
+	Payload []byte
+	Timestamp int64
+}
+
+func NewMessage(offset int64, payload []byte) Message {
+	return Message{
+		Offset: offset,
+		Payload: payload,
+		Timestamp: time.Now().Unix(),
+	}
+}
+
+func (m Message) Size() int {
+	return len(m.Payload)
+}
